d780703be461f16833903ccf1905e955907679ce
Miguel Vergara Martínez <8235478+miguelovergara@users.noreply.github.com>
2026-06-02 13:59:03 -0400

    Add license and Chile localization fallback (#1)
    * Add license and Chile localization fallback
    * Updated header description, changing mexico references for Chile

--- CL.js
+++ CL.js
@@ -1,7 +1,34 @@
+/*
+ * CL.js -- WME Validator Localization for Chile
+ * Copyright (C) 2026 @miguelovergara
+ *
+ * This file is part of WME Validator: https://github.com/WMEValidator/
+ *
+ * WME Validator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WME Validator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WME Validator. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /*************************************************************************
+  * This file falls back to ES (spanish) localization
+  *************************************************************************/
+
 ".country": "Chile",
 ".fallbackCode": "ES",
+".lng": "ES-CL",
 
-"59.enabled": true,
-"150.enabled": true,
-"170.enabled": true,
-"200.enabled": false,
+/* rules pending to review
+"59.enabled": true,   // alerta si una autopista tiene nombre de ciudad
+"150.enabled": true,  // alerta si una autopista no está bloqueada al nivel de usuario requerido
+"170.enabled": true,  // alerta si el nombre de calle comienza con minúscula
+"200.enabled": false, // Suprime alerta de giros no confirmados (suaves) en caminos secundarios
+*/
